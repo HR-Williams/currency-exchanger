7157a34a03a0fa94bef7760b3432541ee962f382
HR Williams <halrubinwilliams@gmail.com>
2021-03-28 02:22:54 -0700

    Get rid of spaces that were accidnetally added to fetch http that broke code

--- src/currency-changer.js
+++ src/currency-changer.js
@@ -1,6 +1,6 @@
 export default class CurrencyChanger {
   static getRate(currency, amount) {
-    return fetch(`https://v6.   exchangerate-api.com/v6/${process.env.API_KEY}/pair/USD/${currency}/${amount}`)
+    return fetch(`https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/pair/USD/${currency}/${amount}`)
       .then(function(response) {
         if (!response.ok) {
           throw Error(response.status.Text);
