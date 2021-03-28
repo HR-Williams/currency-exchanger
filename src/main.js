5802170ed7f3d6479ef37dbc5ecda86f44d75b9f
HR Williams <halrubinwilliams@gmail.com>
2021-03-28 01:19:31 -0700

    Change text in error message

--- src/main.js
+++ src/main.js
@@ -5,16 +5,16 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
 
 function clearFields() {
-  // $('#currency').option("selected");
   $('.showErrors').text("");
   $('.showRate').text("");
+
 }
 
 function getElements(response) {
   if (response.result === "success") {
     $('.showRate').text(`Your USD query is ${response.conversion_result} ${response.target_code}`);
   } else if (response["error-type"] === "unsupported-code") {
-    $('.showResultError').text("Unfortunatly the matrix (the universe? do we know?) can't show you the conversion rate right now. It is beyond your control, just like the market itself. Money is made up but unfortunately capitalism is real and we live in it. If possible try to throw aside the concept of scarcity. If those of us vibrating on the same wave, tune in to each other's frequency we could all live in abundance.")
+    $('.showErrors').text("You chose Agama Dollars, the currency on the Lizard Planet. Unfortunatly the matrix (the universe? do we know?) can't show you that conversion rate right now. It is beyond your control, just like the market itself. Money is made up but unfortunately capitalism is real and we live in it. If possible try to throw aside the concept of scarcity. If those of us vibrating on the same wave, tune in to each other's frequency we could all live in abundance. Or if you are too beat down for that just play the game and choose a currency from planet Earth.");
   } else {
     $('.showErrors').text(`There was an error: ${response.message}. Please enter a number.`);
   } 
@@ -26,7 +26,7 @@ $(document).ready(function() {
     let currency = $('#currency').val();
     clearFields();
     CurrencyChanger.getRate(currency, amount).then(function(response) {
-      getElements(response)
+      getElements(response);
     });
   });
 });
