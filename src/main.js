1733324bd251fe4f44f2648490eb17da32b27eb1
HR Williams <halrubinwilliams@gmail.com>
2021-03-28 09:04:55 -0700

    Change error message text

--- src/main.js
+++ src/main.js
@@ -14,7 +14,7 @@ function getElements(response) {
   if (response.result === "success") {
     $('.showRate').text(`Your USD query is ${response.conversion_result} ${response.target_code}`)
   } else if (response["error-type"] === "unsupported-code") {
-    $('.showResultErrors').text("You chose Agama Dollars, the currency on the Lizard Planet. Unfortunatly the matrix (the universe? do we know?) can't show you that conversion rate right now. It is beyond your control, just like the market itself. Money is made up but unfortunately capitalism is real and we live in it. If possible try to throw aside the concept of scarcity. If those of us vibrating on the same wave, tune in to each other's frequency we could all live in abundance. Or if you are too beat down for that just play the game and choose a currency from planet Earth.")
+    $('.showResultErrors').text("We asked the market to give us the current rate of USD to Agama Dollars, the currency on the Lizard Planet. Unfortunatly the matrix (the universe? do we know?) can't show you that conversion rate right now. It is beyond your control, just like the market itself. Money is made up but unfortunately capitalism is real and we live in it. If possible try to throw aside the concept of scarcity. If those of us vibrating on the same wave, tune in to each other's frequency we could all live in abundance. Or if you are too beat down for that just play the game and choose a currency from planet Earth.")
   } else {
     $('.showErrors').text(`There was an error: ${response.message}. Please enter a number.`);
   } 
