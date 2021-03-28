b3c9ef3e5dec7923b63356717cbc6c32d39c96c6
HR Williams <halrubinwilliams@gmail.com>
2021-03-28 01:17:03 -0700

    Fix indentation as linter specified

--- src/currency-changer.js
+++ src/currency-changer.js
@@ -1,14 +1,14 @@
 export default class CurrencyChanger {
   static getRate(currency, amount) {
-    return fetch(`https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/pair/USD/${currency}/${amount}`)
-    .then(function(response) {
-      if (!response.ok) {
-        throw Error(response.status.Text);
-      }
-      return response.json();
-    })
-    .catch(function(error) {
-      return error;
-    })
+    return fetch(`https://v6.   exchangerate-api.com/v6/${process.env.API_KEY}/pair/USD/${currency}/${amount}`)
+      .then(function(response) {
+        if (!response.ok) {
+          throw Error(response.status.Text);
+        }
+        return response.json();
+      })
+      .catch(function(error) {
+        return error;
+      });
   }
 }
