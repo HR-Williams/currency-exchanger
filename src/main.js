fb07c6e089db932e4f5ac813d54ac575758ef9a3
HR Williams <halrubinwilliams@gmail.com>
2021-03-27 23:01:06 -0700

    Fix clearErrors function, change showRate & showErrors text, fix amount variable to take input, add amount as argument to getRate

--- src/main.js
+++ src/main.js
@@ -5,25 +5,26 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
 
 function clearFields() {
-  $('#currency').selected("selected");
-  $('.show-errors').text("")
+  // $('#currency').option("selected");
+  $('.showErrors').text("");
+  $('.showRate').text("");
 }
 
 function getElements(response) {
   if (response.result === "success") {
-    $('.showRate').text(`${amount}USD is ${response.conversion_result} ${currency}`);
+    $('.showRate').text(`Your USD query is ${response.conversion_result} ${response.target_code}`);
   } else if (response.result === "error") { $('.showResponseError').text("Unfortunatly the matrix (the universe? do we know?) can't show you the conversion rate right now. It is beyond your control, just like the market itself. Money is made up but unfortunately capitalism is real and we live in it. If possible try to throw aside the concept of scarcity. If those of us vibrating on the same wave, tune in to each other's frequency we could all live in abundance.")
   } else {
-    $('.showErrors').text(`There was an error: ${response.message}`);
+    $('.showErrors').text(`There was an error: ${response.message}. Please enter a number.`);
   } 
 }
 
 $(document).ready(function() {
   $('#currencyAmount').click(function() {
-    let amount = parseInt($('#amount').val());
+    let amount = parseInt($('input#amount').val());
     let currency = $('#currency').val();
     clearFields();
-    CurrencyChanger.getRate(currency).then(function(response) {
+    CurrencyChanger.getRate(currency, amount).then(function(response) {
       getElements(response)
     });
   });
